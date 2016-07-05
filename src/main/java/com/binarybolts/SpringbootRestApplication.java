39d22c529a65c160c633dfdfdaaa69fd95c65f76
rfsbsb <rafael@rafaelsilva.net>
2016-07-04 21:06:58 -0300

    Initial commit

---
+++ src/main/java/com/binarybolts/SpringbootRestApplication.java
@@ -0,0 +1,12 @@
+package com.binarybolts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApplication.class, args);
+	}
+}
